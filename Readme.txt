288e41cd760489e779257c0c84ac57b8ae8765e2
PrachiPoddar <prachi99poddar@gmail.com>
2022-12-30 19:36:30 +0530

    Added readme

--- Readme.txt
+++ Readme.txt
@@ -1,5 +1,26 @@
+# Project setup:
 
-Check coverage
-```
-go test -coverprofile=coverage.out
-```
+step 1: Install Go from https://go.dev, to verify enter the following command in terminal :-  "go version"
+
+step 2: Install Go extension in vscode 
+
+step 3: Provide your mysql db connection string in the .env file in root folder or insert your user and password in the following line and paste it in .env file
+"DSN=<username>:<password>@tcp(localhost:3306)/<schemaName>?charset=utf8mb4&parseTime=True&loc=Local"
+
+
+# Run the application:
+
+To run the application, open this folder in integrated terminal by double clicking on the main.go file in root directory and enter command, "go run main.go"
+
+Use command "go build" to build the application
+
+Note: Do install the dependency packages that vscode suggests you to download like "gopls", if it does not suggests run the following command in integrated terminal:
+
+"go install -v golang.org/x/tools/gopls@latest"
+
+# Run tests:
+
+Go to _test.go file in respective folders and click on run test button appearing on top of test functions.
+
+
+
--- service/service_test.go
+++ service/service_test.go
@@ -2,18 +2,68 @@ package service
 
 import (
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestFormatDuration(t *testing.T) {
-	expected := "45h2m"
-	result := formatDuration(450234)
-	assert.Equal(t, expected, result)
+	tests := []struct {
+		name     string
+		input    int
+		expected string
+	}{
+		{
+			name:     "value 0",
+			input:    0,
+			expected: "0h0m",
+		},
+		{
+			name:     "value length greater than 2",
+			input:    25,
+			expected: "25s",
+		},
+		{
+			name:     "value length greater than 4",
+			input:    451624,
+			expected: "45h16m",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := formatDuration(tt.input)
+			if result != tt.expected {
+				t.Errorf("Expected: %s, Actual  %s", tt.expected, result)
+			}
+		})
+	}
 }
 
 func TestFormatSize(t *testing.T) {
-	expected := "43.5MB"
-	result := formatSize(43534553)
-	assert.Equal(t, expected, result)
+	tests := []struct {
+		name     string
+		input    float64
+		expected string
+	}{
+		{
+			name:     "value greater than mb",
+			input:    43534553,
+			expected: "43.5MB",
+		},
+		{
+			name:     "value greater than gb",
+			input:    43534534234,
+			expected: "43.5GB",
+		},
+		{
+			name:     "value greater than tb",
+			input:    41253453428734,
+			expected: "41.3TB",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := formatSize(tt.input)
+			if result != tt.expected {
+				t.Errorf("Expected: %s, Actual  %s", tt.expected, result)
+			}
+		})
+	}
 }
